6690ef266c2c63418e929ad96136f36991d79a36
antenor-z <antenor-z@pm.me>
2024-04-30 10:33:06 -0300

    drawing the runway

--- static/wind.js
+++ static/wind.js
@@ -1,25 +1,43 @@
 function calculateWind() {
-    const runwayHead = document.getElementById("runway-head").value
-    const windDir = document.getElementById("wind-dir").value
-    const windSpeed = document.getElementById("wind-speed").value
-    calculateWindReq(windDir, windSpeed, runwayHead)
-    .then(result => {
-      console.log(result)
-      document.getElementById("w").getElementById("angle").textContent = result.angle
-      document.getElementById("w").getElementById("cross").textContent = result.cross
-      document.getElementById("w").getElementById("head").textContent = result.head
-    });
-  }
-  function calculateWindReq(wind_dir, wind_speed, runway_head) {
-    const apiUrl = `/windcalc/?wind_dir=${wind_dir}&wind_speed=${wind_speed}&runway_head=${runway_head}`
+  const runwayHead = document.getElementById("runway-head").value
+  const windDir = document.getElementById("wind-dir").value
+  const windSpeed = document.getElementById("wind-speed").value
+  calculateWindReq(windDir, windSpeed, runwayHead)
+  .then(result => {
+    console.log(result)
 
-    return fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        return data
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error)
-        return null
-      });
+    let head = result.head * 10
+    let cross = result.cross * 10
+    let speed = windSpeed * 10
+
+    document.getElementById("vector-head").setAttribute("height", Math.abs(head))
+    if (head < 0)
+      document.getElementById("vector-head").setAttribute('transform', 'rotate(' + 180 + ' 0 0)')
+    else 
+    document.getElementById("vector-head").setAttribute('transform', 'rotate(' + 0 + ' 0 0)')
+  
+
+    document.getElementById("vector-cross").setAttribute("height", Math.abs(cross))
+    if (cross < 0)
+      document.getElementById("vector-cross").setAttribute('transform', 'rotate(' + 270 + ' 0 0)')
+    else
+    document.getElementById("vector-cross").setAttribute('transform', 'rotate(' + 90 + ' 0 0)')
+
+    document.getElementById("vector").setAttribute('transform', 'rotate(' + result.angle + ' 0 0)')
+    document.getElementById("vector").setAttribute("height", Math.abs(speed))
+
+  })
   }
+function calculateWindReq(wind_dir, wind_speed, runway_head) {
+  const apiUrl = `/windcalc/?wind_dir=${wind_dir}&wind_speed=${wind_speed}&runway_head=${runway_head}`
+
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      return data
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error)
+      return null
+    });
+}
