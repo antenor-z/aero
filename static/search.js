5640d548580a539ccfd285138669b7c5c1c2de13
Antenor Leal <antenor.leal@oceanpact.com>
2024-07-10 14:06:44 -0300

    feat: pressing <enter> to access airport if only one result is being shown

--- static/search.js
+++ static/search.js
@@ -3,8 +3,7 @@ function searchAirports() {
 
     var airports = document.getElementsByClassName('airport')
 
-    let resultFound = false
-
+    var visibleCount = 0
     for (var i = 0; i < airports.length; i++) {
         var airport = airports[i]
 
@@ -14,16 +13,41 @@ function searchAirports() {
 
         if (city.includes(query) || icao.includes(query) || airportName.includes(query)) {
             airport.style.display = 'block'
-            resultFound = true
+            visibleCount += 1
         } else {
             airport.style.display = 'none'
         }
+    }
 
-        var noResultMessage = document.getElementById('noResultMessage')
-        if (resultFound) {
-            noResultMessage.style.display = 'none'
-        } else {
-            noResultMessage.style.display = 'block'
-        }
+    var noResultMessage = document.getElementById('noResultMessage')
+    if (visibleCount > 0) {
+        noResultMessage.style.display = 'none'
+    } else {
+        noResultMessage.style.display = 'block'
+    }
+
+    var enterMessage = document.getElementById('enterMessage')
+    if (visibleCount === 1) {
+        enterMessage.style.display = 'block'
+    } else {
+        enterMessage.style.display = 'none'
     }
 }
+
+document.getElementById('searchInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        var airports = document.getElementsByClassName('airport')
+        var visibleAirports = [];
+
+        for (var i = 0; i < airports.length; i++) {
+            if (airports[i].style.display !== 'none') {
+                visibleAirports.push(airports[i])
+            }
+        }
+
+        if (visibleAirports.length === 1) {
+            var icaoCode = visibleAirports[0].querySelector('.box-value').textContent.split(' ● ')[0]
+            window.location.href = '/info/' + icaoCode
+        }
+    }
+})
