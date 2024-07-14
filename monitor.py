7238b061f6e45e3f4aea4e684312ded29ae3aff4
antenor-z <antenor-z@pm.me>
2024-07-14 17:03:19 -0300

    print options

--- monitor.py
+++ monitor.py
@@ -1,13 +1,22 @@
+from DB.Getter import get_all_icao
 from DB.addUser import add_user
 from DB.PortableDataLoad import insert_initial_data
+from ext import update_metars, update_tafs
+
+def update_all_metars(): update_metars(get_all_icao())
+def update_all_tafs(): update_tafs(get_all_icao())
+def exit_now(): exit()
+
 while(True):
-    print("0: Exit")
-    print("1: Insert initial data")
-    print("2: Insert user")
-    option = input("Option: ")
-    if option == "0": break
     options = {
+        "0": exit_now,
         "1": insert_initial_data,
-        "2": add_user
+        "2": add_user,
+        "3": update_all_metars,
+        "4": update_all_tafs
     }
+    for num, func in options.items():
+        print(f"{num}: {func.__name__}")
+
+    option = input("Option: ")
     options.get(option, None)()
