6efcbbf5b342cd41112553ade382361aa2172252
Antenor Leal <antenor.leal@oceanpact.com>
2024-08-22 12:46:48 -0300

    ploting svg with units

--- historyPlot.py
+++ historyPlot.py
@@ -12,10 +12,13 @@ def plot_metar_data(icao: str,
                     metar_data: list[dict],
                     data_name1: str,
                     label_name1: str,
+                    unit1: str,
                     label_color1: str,
                     data_name2: str,
                     label_name2: str,
-                    label_color2: str):
+                    unit2: str,
+                    label_color2: str
+                    ):
 
     with plt.rc_context({'axes.edgecolor':'white', 'xtick.color':'white', 'ytick.color':'green', 'figure.facecolor':'#333'}):    
         timestamps = [(data["timestamp"] - timedelta(hours=3)).strftime("%H:%M") for data in metar_data if data]
@@ -31,8 +34,8 @@ def plot_metar_data(icao: str,
         ax1.tick_params(axis='y', color='white', labelcolor=f'{label_color1}')
 
         for i, value in enumerate(data1):
-            ax1.annotate(f'{value}', xy=(timestamps[i], value), xytext=(0, 5), textcoords='offset points',
-                            ha='center', color=label_color1, fontsize=9)
+            ax1.annotate(f'{value} {unit1}', xy=(timestamps[i], value), xytext=(0, 5), textcoords='offset points',
+                         ha='center', color=label_color1, fontsize=9)
 
         ax2 = ax1.twinx()
         ax2.set_ylabel(label_name2, color=label_color2)
@@ -40,13 +43,13 @@ def plot_metar_data(icao: str,
         ax2.tick_params(axis='y', color='white', labelcolor=label_color2)
 
         for i, value in enumerate(data2):
-            ax2.annotate(f'{value}', xy=(timestamps[i], value), xytext=(0, -12), textcoords='offset points',
-                        ha='center', color=label_color2, fontsize=9)
+            ax2.annotate(f'{value} {unit2}', xy=(timestamps[i], value), xytext=(0, -12), textcoords='offset points',
+                         ha='center', color=label_color2, fontsize=9)
 
         # ax1.grid(True)
 
         fig.tight_layout()
-        filename = f"static/plots/{icao}-{data_name1}-{data_name2}.png"
+        filename = f"static/plots/{icao}-{data_name1}-{data_name2}.svg"
         plt.savefig(filename, dpi=600)
         plt.close(fig)
         print(f"Plot saved as {filename}")
@@ -55,9 +58,9 @@ def plot_metar_data(icao: str,
 def plot(icao, metar_data):
     BLUE = "#008ae6"
     YELLOW = "#d5a810"
-    plot_metar_data(icao, metar_data, "temperature", "Temperatura (°C)", BLUE, "dew_point", "Ponto de orvalho (°C)", YELLOW)
-    plot_metar_data(icao, metar_data, "wind_speed", "Velocidade do vento (kt)", BLUE, "wind_direction", "Direção do vento (graus)", YELLOW)
-    plot_metar_data(icao, metar_data, "qnh", "Ajuste altímetro (hPa)", BLUE, "visibility", "Visibilidade (m)", YELLOW)
+    plot_metar_data(icao, metar_data, "temperature", "Temperatura (°C)", "°C", BLUE, "dew_point", "Ponto de orvalho (°C)", "°C", YELLOW)
+    plot_metar_data(icao, metar_data, "wind_speed", "Velocidade do vento (kt)", "kt", BLUE, "wind_direction", "Direção do vento (graus)", "°", YELLOW)
+    plot_metar_data(icao, metar_data, "qnh", "Ajuste altímetro (hectopascal)", "hPa", BLUE, "visibility", "Visibilidade (metros)", "m", YELLOW)
 
 
 def update_images():
