d391adc340c1b6e2102a21a8f9489b5205d25add
Antenor Leal <antenor.leal@oceanpact.com>
2024-09-23 13:30:26 -0300

    error messages

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from os import environ
 from fastapi import FastAPI, HTTPException, Request, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.exceptions import RequestValidationError
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from DB.Getter import get_all_icao, get_all_names, get_info, latest_n_metars_parsed
@@ -66,7 +67,7 @@ async def info(request: Request, icao: str):
     try:
         info = get_info(icao)
     except Exception:
-        raise HTTPException(status_code=400, detail="Aeroporto não encontrado")
+        raise HTTPException(status_code=404, detail="Aeroporto não encontrado")
 
     try:
         metar = get_metar(icao)
@@ -74,15 +75,12 @@ async def info(request: Request, icao: str):
     except Exception:
         decoded = [("", "Não foi possível obter o METAR")]
 
-    history = latest_n_metars_parsed(icao=icao, n=10)
-
     return templates.TemplateResponse("airport.html", 
                                       {"request": request, 
                                        "info": info, 
                                        "icao": icao, 
                                        "metar": metar, 
-                                       "decoded": decoded, 
-                                       "history": history})
+                                       "decoded": decoded})
 
 @app.get("/taf/{icao}", response_class=HTMLResponse)
 async def info_taf(request: Request, icao: str):
@@ -92,6 +90,10 @@ async def info_taf(request: Request, icao: str):
 
     try:
         taf = get_taf(icao)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Aeroporto não encontrado")
+
+    try:
         info = get_info(icao)
         decoded = decode_taf(taf)
     except Exception:
@@ -126,12 +128,34 @@ async def descent(request: Request):
 
 @app.get("/history/{icao}/", response_class=HTMLResponse)
 async def history(request: Request, icao: str):
-    info = get_info(icao)
+    try:
+        info = get_info(icao)
+    except Exception:
+        raise HTTPException(status_code=404, detail="Aeroporto não encontrado")
     return templates.TemplateResponse("history.html", {"request": request, "icao": icao, "info": info})
 
 @app.exception_handler(404)
 async def not_found(request: Request, exc: HTTPException):
-    return templates.TemplateResponse("error.html", {"request": request, "error": "404 | Página não encontrada."}, status_code=404)
+    return templates.TemplateResponse("error.html", {"request": request, "error": f"Erro 404 | {exc.detail}"}, status_code=404)
+
+@app.exception_handler(400)
+async def bad_request(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("error.html", {"request": request, "error": f"Erro 400 | {exc.detail}"}, status_code=400)
+
+@app.exception_handler(401)
+async def unauthorized(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("error.html", {"request": request, "error": f"Erro 401 | {exc.detail}"}, status_code=401)
+
+@app.exception_handler(RequestValidationError)
+async def unprocesable(request: Request, exc: RequestValidationError):
+    details = []
+    for error in exc.errors():
+        details.append(f"- {error["loc"][1]} = {error["input"]}: {error["msg"]}")
+    return templates.TemplateResponse("error.html", {"request": request, 
+                                                     "error": f"Erro 422 | Erro no formulário", 
+                                                     "details": details},
+                                                     status_code=401)
+
 
 @app.exception_handler(NotLoggedException)
 async def not_logged_exception_handler(request: Request, exc: NotLoggedException):
