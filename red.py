b7536fe4072885b6305278aa63a800dd2c20e56b
antenor.leal <antenor.leal@oceanpact.com>
2024-10-03 14:37:39 -0300

    ?

--- red.py
+++ red.py
@@ -1,15 +1,41 @@
+from datetime import datetime
 import redis, json
 
 client = redis.Redis(host='redis', port=6379, decode_responses=True)
 
+def datetime_serializer(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError(f"Type {type(obj)} not serializable")
+
+def datetime_parser(dct):
+    for key, value in dct.items():
+        try:
+            dct[key] = datetime.fromisoformat(value)
+        except (ValueError, TypeError):
+            pass
+    return dct
+
 def cache_it(func):
     def wrapper(*args, **kargs):
-        icao = kargs.get("icao") or args[0: 1] or "default"
+        try:
+            icao = kargs.get("icao") or args[0]
+        except IndexError:
+            icao = "default"
         key = f'{icao}:{func.__name__}'
-        cached = json.loads(client.get(key) or "null")
+        print(key, end=" :: ")
+        cached = json.loads(client.get(key) or "null", object_hook=datetime_parser)
         if not cached:
+            print("Vou tentar chamar a funcao")
             cached = func(*args, **kargs)
-            client.set(key, json.dumps(cached))
+            print("saí da função")
+            print(cached)
+            print(json.dumps(cached))
+            print(json.loads(cached, default=datetime_serializer))
+            client.set(key, json.dumps(cached, default=datetime_serializer))
+            print("miss", cached)
+        else:
+            print("hit", cached)
         return cached            
     return wrapper
 
